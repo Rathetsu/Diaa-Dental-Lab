6cc56b64a2c6e9e1b36ab2641cfd8f514344037d
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2024-01-27 03:11:28 +0200

    initial commit

---
+++ src/components/app-icon/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const AppIcon: React.FC = () => {
+  return (
+	<img
+	  src="/logo_.png"
+	  alt="Al Diaa Dental Lab"
+	  style={{ width: "300px", height: "90px" }}
+	/>
+  );
+};
